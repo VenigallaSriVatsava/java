f404c302d2497038325293aeac50911e8c4a6b22
SriVatsava <vatsavvenigalla2463@gmail.com>
2022-02-01 20:09:41 +0530

    initial commit

---
+++ src/main/java/Problems/BasketProblem.java
@@ -0,0 +1,42 @@
+package Problems;
+
+import java.util.Scanner;
+public class BasketProblem {
+
+
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            //   System.out.println("enter the number of baskets");
+            int numberOfBaskets = 5;
+
+            int count = 0;
+
+            int[] basketCapacity = {11,20,15,17,34};
+      /* System.out.println("each baskets capacity");
+        for (int i = 0; i < numberOfBaskets; i++) {
+            basketCapacity[i] = sc.nextInt();
+        }*/
+            System.out.println("no of apples");
+            int numberOfApples = 55;
+            for (int k = 0; k < numberOfBaskets; k++) {
+                int remainingApples = numberOfApples % basketCapacity[k];
+                if (remainingApples == 0) {
+                    count++;
+                } else {
+                    for (int j = 0; j < numberOfBaskets; j++) {
+                        if (remainingApples >= basketCapacity[j]) {
+                            int remainingApples1 = remainingApples % basketCapacity[j];
+                            if (remainingApples1 == 0) {
+                                count++;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+            System.out.println("number of possible ways: " +count);
+        }
+    }
--- src/main/java/operators/AssignmentOperator.java
+++ src/main/java/operators/AssignmentOperator.java
@@ -1,4 +1,4 @@
-package operator;
+package operators;
 
 public class AssignmentOperator {
 
