350e19a0e338f29ab9693a2c6efa4ab8b8fcb25c
SriVatsava <vatsavvenigalla2463@gmail.com>
2022-02-01 20:17:27 +0530

    initial commit

---
+++ src/main/java/ControlLoops/ControlLoops.java
@@ -0,0 +1,103 @@
+package ControlLoops;
+
+public class ControlLoops {
+
+
+        /* IF LOOP
+           if(condition)
+               Statement1;
+           else
+               Statement2;
+               */
+        // example
+        int a=9,b=8;
+
+    if(a>b)
+        {
+            System.out.println("a>b");
+        }
+    else
+
+        {
+            System.out.println("a<b");
+        }
+
+    /* NESTED IF
+       if(conditionA1) {
+          if(conditionA11) {
+              Statement1; }
+
+          else(conditionA12) {
+              Statement2; }
+
+      else(conditionB1) {
+          Statement3; }
+          */
+
+        int i,j,k,c;
+
+    if(i==10) {
+            if(j==20) {
+                a = b;
+            }
+            if(j>20) {
+                a = c;
+            }
+            else {
+                a = b+c;
+            }
+        }
+    else{
+            c = b;
+        }
+
+    /* IF-ELSE-IF LADDER
+       if(condition) {
+       Statement1; }
+       elseif(condition2) {
+       Statement2; }
+       elseif(condition3) {
+       Statement3; }
+       ...
+        ...
+        ...
+        else {
+        StatementN; }
+     */
+
+    if(a=1) {
+            System.out.println("one"); }
+    else if(a=2) {
+            System.out.println("two"); }
+    else {
+            System.out.println("n"); }
+
+    /* SWITCH
+     switch(expression) {
+     case value1;
+     statement;
+     break;
+     case value2;
+     statement;
+     break;
+     ..
+     ..
+     case valueN;
+     statement;
+     break;
+     default:
+     default statement
+     */
+
+        // example
+
+
+
+
+
+
+
+
+
+    }
+
